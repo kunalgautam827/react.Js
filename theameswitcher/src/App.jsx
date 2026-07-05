5349d86a31ebbe267483c87905fb98901a876cfe
kunal gautam <kunalgautam827@gmail.com>
2026-07-05 22:33:48 +0530

    context API with another way,create & provide in single file

---
+++ theameswitcher/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/Theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode,setThemeMode]=useState("light")
+  function lightTheme(){
+    setThemeMode("light")
+  }
+  function darkTheme(){
+    setThemeMode("dark")
+  }
+  useEffect(()=>{
+    let mode = document.querySelector('html').classList
+    mode.remove("light","dark")
+    mode.add(themeMode)
+  },[themeMode]);
+
+  return (
+    <>
+      <ThemeProvider value={{themeMode , darkTheme , lightTheme} }>
+        <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
