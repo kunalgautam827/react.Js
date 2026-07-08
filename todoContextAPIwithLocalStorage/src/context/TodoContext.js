1c92355915095e4e41dd74176977f8c3c7435d03
kunal gautam <kunalgautam827@gmail.com>
2026-07-08 11:02:07 +0530

    done context API with local storage

--- todoContextAPIwithLocalStorage/src/context/TodoContext.js
+++ todoContextAPIwithLocalStorage/src/context/TodoContext.js
@@ -1,23 +1,21 @@
-import { createContext , useContext } from "react";
+import { createContext, useContext } from "react";
 
-export const TodoContext = createContext(
+export const TodoContext = createContext({
+  todos: [
     {
-        todos : [
-            {
-                todo : "todo message",
-                id : 1,
-                completed : false,
-            }
-        ],
-        addTodo : (todo) =>{},
-        updateTodo : (id,todo)=>{},
-        deleteTodo : (id)=>{},
-        toggleComplete : (id)=>{}
-    }  
-)
+      todo: "todo message",
+      id: 1,
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 
-export const TodoProvider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider;
 
-export const useTodo = ()=>{
-    return useContext(TodoContext);
-}
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
