73fb97ee7cf575cad882abaf590bc71175c73932
kunal gautam <kunalgautam827@gmail.com>
2026-07-29 15:05:26 +0530

    more components - Select,PostCard and AuthLayout

---
+++ megaBlogs/src/compoents/AuthLayout.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Protected({ childern, authentication = true }) {  // authentication → Whether this page requires login.
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  const [loder, setLoder] = useState(true);
+
+  useEffect(() => {
+    if (authentication && authentication !== authStatus) {
+      navigate("/login");
+    } else if (!authentication && authentication !== authStatus) {
+      navigate("/");
+    }
+    setLoder(false)
+  }, [navigate, authStatus, authentication]);
+  return loder ? <h1>Loging...</h1> : <>{childern}</>;
+}
---
+++ megaBlogs/src/compoents/Select.jsx
@@ -0,0 +1,25 @@
+import React, { forwardRef, useId } from "react";
+
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id}>{label}</label>}
+      <select
+        name={options}
+        id={id}
+        {...props}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 divide-neutral-200 border border-gray-200 w-full ${className}`}
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default forwardRef(Select);
